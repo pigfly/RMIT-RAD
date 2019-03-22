06ba8b762a2db1071d1589add5c39f7f8a3a04af
Alex Jiang <pigflyjky@gmail.com>
2019-03-22 03:29:03 +0000

    [week2-c9](rails): root index to users#index

--- week2/toy_app/config/routes.rb
+++ week2/toy_app/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'users#index'
 end
