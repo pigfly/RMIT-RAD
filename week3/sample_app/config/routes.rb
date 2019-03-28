c312ec667a07978f8a7c9edb53c2f3422d16a946
Alex Jiang <pigflyjky@gmail.com>
2019-03-28 00:24:33 +0000

    [week3-rails](md): add static_pages app

---
+++ week3/sample_app/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#hello'
+end
