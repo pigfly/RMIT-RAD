8cbbb2c694dd3dc49d85030a8cc25c72e93321f3
Alex Jiang <pigflyjky@gmail.com>
2019-03-28 05:26:12 +0000

    week3-rails](sample_app): refactor to use application template

--- week3/sample_app/config/routes.rb
+++ week3/sample_app/config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'static_pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
 end
