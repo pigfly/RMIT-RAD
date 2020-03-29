0bb25cc59647973cf3e658c772b6bc3a0aa356b1
Alex Jiang <pigflyjky@gmail.com>
2020-03-29 13:15:54 +1100

    [week4-rails](docker): week4 demo route path

--- 2020_s1/week4/config/routes.rb
+++ 2020_s1/week4/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get  'static_pages/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
