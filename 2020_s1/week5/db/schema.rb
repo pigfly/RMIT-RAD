73f1951a60dd7fa3be3352049edcc1209af0d6ed
Alex Jiang <pigflyjky@gmail.com>
2020-04-03 20:53:39 +1100

    [week5-rails](docker): user model

--- 2020_s1/week5/db/schema.rb
+++ 2020_s1/week5/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_04_03_094848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
