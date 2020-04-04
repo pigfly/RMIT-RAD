d141292923c9d9dbec4c5051a3c17dee5cd028b7
Alex Jiang <pigflyjky@gmail.com>
2020-04-04 22:32:21 +1100

    [week5-rails](docker): user model

--- 2020_s1/week5/app/models/user.rb
+++ 2020_s1/week5/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :email, presence: true, length: { maximum: 511 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 end
