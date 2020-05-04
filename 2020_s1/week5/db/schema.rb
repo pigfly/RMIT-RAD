e985b5150176e5a95ed02a1dc5a6bfd68a7a234b
Alex Jiang <pigflyjky@gmail.com>
2020-05-04 20:58:28 +1000

    [week7-rails](rails): user model

--- 2020_s1/week5/db/schema.rb
+++ 2020_s1/week5/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_094848) do
+ActiveRecord::Schema.define(version: 2020_05_04_105337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_04_03_094848) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
