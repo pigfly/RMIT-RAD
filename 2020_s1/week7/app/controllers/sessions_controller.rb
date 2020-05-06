c3238a20ae8640554c9306a9906f9362fd3e1028
Alex Jiang <pigflyjky@gmail.com>
2020-05-06 15:15:28 +1000

    [week8-rails](rails): login page

---
+++ 2020_s1/week7/app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create 
+    render 'new'
+  end
+
+  def destroy
+  end
+end
--- 2020_s1/week7/config/routes.rb
+++ 2020_s1/week7/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
@@ -6,5 +7,8 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users
 end
