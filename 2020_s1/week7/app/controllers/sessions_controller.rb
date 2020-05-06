0fb43cbdf700fe3624f0c426b13069d4a9837e44
Alex Jiang <pigflyjky@gmail.com>
2020-05-06 17:01:07 +1000

    [week8-rails](rails): logout cleanup

--- 2020_s1/week7/app/controllers/sessions_controller.rb
+++ 2020_s1/week7/app/controllers/sessions_controller.rb
@@ -14,5 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
--- 2020_s1/week7/app/models/user.rb
+++ 2020_s1/week7/app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
